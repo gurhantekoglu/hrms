2aca647aa230ef586ad4c58e4a1cea062d4d5af8
gurha <gurha@LAPTOP-2OVS8O15>
2021-06-11 02:54:22 +0300

    new entities added

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/Candidate.java
@@ -0,0 +1,34 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "candidates")
+@PrimaryKeyJoinColumn(name = "user_id")
+public class Candidate extends User {
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "nationality_id")
+	private String nationalityId;
+
+	@Column(name = "year_of_birth")
+	private LocalDate yearOfBirth;
+
+}
---
+++ src/main/java/kodlamaio/hrms/entities/concretes/Employer.java
@@ -0,0 +1,29 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "employers")
+@PrimaryKeyJoinColumn(name = "user_id")
+public class Employer extends User {
+
+	@Column(name = "company_name")
+	private String companyName;
+
+	@Column(name = "website")
+	private String website;
+
+	@Column(name = "phone_number")
+	private String phoneNumber;
+
+}
